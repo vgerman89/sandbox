4b94fc55e74e9d38b437dce8c50fccc2dbad843e
vgerman <vgermanrus@gmail.com>
2018-08-12 15:00:01 +0300

    создан тест модификации группы

---
+++ addressbook-web-tests/src/test/java/svz/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,19 @@
+package svz.addressbook.tests;
+
+
+import org.testng.annotations.Test;
+import svz.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase {
+
+  @Test
+  public void testGroupModification() {
+  app.getNavigationHelper().gotoGroupPage();
+  app.getGroupHelper().selectGroup();
+  app.getGroupHelper().initGroupModification();
+  app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+  app.getGroupHelper().submitGroupModification();
+  app.getGroupHelper().returnToGroupPage();
+  }
+
+}
