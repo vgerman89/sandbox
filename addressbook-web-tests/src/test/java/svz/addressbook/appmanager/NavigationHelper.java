1730d7ab740a6b7eb4e56195b4eea3961cc9db76
vgerman <vgermanrus@gmail.com>
2018-08-19 16:22:05 +0300

    Оптимизированы переходы между страницами

--- addressbook-web-tests/src/test/java/svz/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/svz/addressbook/appmanager/NavigationHelper.java
@@ -7,14 +7,22 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class NavigationHelper extends HelperBase {
 
   public NavigationHelper(WebDriver wd) {
-    super  (wd);
+    super(wd);
   }
 
   public void gotoGroupPage() {
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+            && isElementPresent(By.name("new"))) {
+      return;
+    }
     click(By.linkText("groups"));
   }
 
-  public void gotoHomePage(){
+  public void gotoHomePage() {
+    if (isElementPresent(By.id("maintable"))) {
+      return;
+    }
     click(By.linkText("home"));
   }
 }
