953a6ed8ad7259c1945523baed87f7d7e574743a
vgerman <vgermanrus@gmail.com>
2018-08-19 17:20:49 +0300

    Добавил предусловия в тест модификации контакта

--- addressbook-web-tests/src/test/java/svz/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/svz/addressbook/tests/ContactModificationTests.java
@@ -9,6 +9,9 @@ public class ContactModificationTests extends TestBase {
   @Test
   public void ContactModificationTests() {
     app.getNavigationHelper().gotoHomePage();
+    if (! app.getContactHelper().isThereAContact()){
+      app.getContactHelper().createContact(new ContactData("Vitaliy", "V.G.", "German", "+79651234567", "vgermanrus@gmail.com", "test1"), true);
+    }
     app.getContactHelper().selectContact();
     app.getContactHelper().initContactModification();
     app.getContactHelper().fillContactForm(new ContactData(
