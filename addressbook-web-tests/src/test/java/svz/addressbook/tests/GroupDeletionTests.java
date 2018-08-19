93b55a4120a65d4817f5d16db1bdd12189a6fc47
vgerman <vgermanrus@gmail.com>
2018-08-19 16:44:54 +0300

    Реализована проверка и обеспечение предусловий

--- addressbook-web-tests/src/test/java/svz/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/svz/addressbook/tests/GroupDeletionTests.java
@@ -1,15 +1,19 @@
 package svz.addressbook.tests;
 
 import org.testng.annotations.Test;
+import svz.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
-    @Test
-    public void testGroupDeletion() {
-      app.getNavigationHelper().gotoGroupPage();
-      app.getGroupHelper().selectGroup();
-      app.getGroupHelper().deleteSelectedGroups();
-      app.getGroupHelper().returnToGroupPage();
+  @Test
+  public void testGroupDeletion() {
+    app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()){
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
     }
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().deleteSelectedGroups();
+    app.getGroupHelper().returnToGroupPage();
+  }
 
 }
