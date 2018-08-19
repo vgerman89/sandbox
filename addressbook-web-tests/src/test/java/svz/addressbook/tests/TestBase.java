b6378174de325f0bb8038d35ee0e3aaff6583f85
vgerman <vgermanrus@gmail.com>
2018-08-19 15:21:40 +0300

    Добавил проверку текста в инпуте

--- addressbook-web-tests/src/test/java/svz/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/svz/addressbook/tests/TestBase.java
@@ -8,7 +8,7 @@ import svz.addressbook.appmanager.ApplicationManager;
 public class TestBase {
 
 
-  protected final ApplicationManager app = new ApplicationManager(BrowserType.EDGE);
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
   @BeforeMethod
   public void setUp() throws Exception {
