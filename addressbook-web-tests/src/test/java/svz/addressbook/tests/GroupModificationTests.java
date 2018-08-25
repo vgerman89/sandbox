cf117e0ac3cf960f1d69a3aa8319c9a53dfdb4bd
vgerman <vgermanrus@gmail.com>
2018-08-25 12:18:32 +0300

    Добавлена проверка количества групп

--- addressbook-web-tests/src/test/java/svz/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/svz/addressbook/tests/GroupModificationTests.java
@@ -1,6 +1,7 @@
 package svz.addressbook.tests;
 
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import svz.addressbook.model.GroupData;
 
@@ -9,6 +10,7 @@ public class GroupModificationTests extends TestBase {
   @Test
   public void testGroupModification() {
     app.getNavigationHelper().gotoGroupPage();
+    int before  = app.getGroupHelper().getGroupCount();
     if (! app.getGroupHelper().isThereAGroup()){
       app.getGroupHelper().createGroup(new GroupData("test1", null, null));
     }
@@ -17,6 +19,8 @@ public class GroupModificationTests extends TestBase {
     app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
+    int after  = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before);
   }
 
 }
