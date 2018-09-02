d809d2b3c6f4e404cda98d67ec932d1d9e5c8fd6
vgerman <vgermanrus@gmail.com>
2018-09-02 15:22:13 +0300

    Реализована првоерка групп в fluent стиле с использованием hamcrest

--- addressbook-web-tests/src/test/java/svz/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/svz/addressbook/appmanager/GroupHelper.java
@@ -4,10 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import svz.addressbook.model.GroupData;
+import svz.addressbook.model.Groups;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class GroupHelper extends HelperBase {
 
@@ -80,8 +79,8 @@ public class GroupHelper extends HelperBase {
     return wd.findElements(By.name("selected[]")).size();
   }
 
-  public Set<GroupData> all() {
-    Set<GroupData> groups = new HashSet<GroupData>();
+  public Groups all() {
+    Groups groups = new Groups();
     List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
     for (WebElement element : elements) {
       String name = element.getText();
