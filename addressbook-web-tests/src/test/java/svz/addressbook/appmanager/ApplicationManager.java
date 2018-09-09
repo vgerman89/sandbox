61f0a360f6bd1758f279802a1ad1817ec1d25042
vgerman <vgermanrus@gmail.com>
2018-09-09 22:55:15 +0300

    Создаем конфигурационный файл local.properties
    а так-же указываем в каком браузере запускать тесты по дефолты

--- addressbook-web-tests/src/test/java/svz/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/svz/addressbook/appmanager/ApplicationManager.java
@@ -7,10 +7,14 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-
+  private final Properties properties;
   WebDriver wd;
 
   private SessionHelper sessionHelper;
@@ -19,11 +23,15 @@ public class ApplicationManager {
   private ContactHelper contactHelper;
   private String browser;
 
-  public ApplicationManager(String browser) {
+  public ApplicationManager(String browser)  {
     this.browser = browser;
+    properties = new Properties();
   }
 
-  public void init() {
+  public void init() throws IOException {
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
     if (browser.equals(BrowserType.FIREFOX)){
       wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
     } else if(browser.equals(BrowserType.CHROME)){
@@ -32,12 +40,12 @@ public class ApplicationManager {
       wd = new EdgeDriver();
     }
     wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook/");
+    wd.get(properties.getProperty("web.baseUrl"));
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
     sessionHelper = new SessionHelper(wd);
     contactHelper = new ContactHelper(wd);
-    sessionHelper.login("admin", "secret");
+    sessionHelper.login(properties.getProperty("web.adminLogin"),properties.getProperty("web.adminPassword"));
   }
 
   public void stop() {
--- addressbook-web-tests/src/test/java/svz/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/svz/addressbook/tests/TestBase.java
@@ -8,7 +8,8 @@ import svz.addressbook.appmanager.ApplicationManager;
 public class TestBase {
 
 
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  protected static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite
   public void setUp() throws Exception {
