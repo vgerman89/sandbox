1a5231797edbd833de640272881a3efae3cc5dd3
vgerman <vgermanrus@gmail.com>
2018-09-16 14:09:09 +0300

    Забираем данные для сравнения групп из БД в тестах групп

--- addressbook-web-tests/src/test/java/svz/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/svz/addressbook/tests/GroupCreationTests.java
@@ -57,9 +57,9 @@ public class GroupCreationTests extends TestBase {
   @Test(dataProvider = "validGroupsFromJson")
   public void testGroupCreation(GroupData group) {
       app.goTo().groupPage();
-      Groups before = app.group().all();
+      Groups before = app.db().groups();
       app.group().create(group);
-      Groups after = app.group().all();
+      Groups after = app.db().groups();
       assertThat(app.group().count(), equalTo(before.size() + 1));
       assertThat(after, equalTo(
               before.withAdded(group.withId(after.stream().mapToInt((g)->g.getId()).max().getAsInt()))));
