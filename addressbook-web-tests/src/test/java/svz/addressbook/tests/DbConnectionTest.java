10a0b8672f4e2de28ca7514e51342787e4829219
vgerman <vgermanrus@gmail.com>
2018-09-16 12:25:11 +0300

    Учимя работать с БД

---
+++ addressbook-web-tests/src/test/java/svz/addressbook/tests/DbConnectionTest.java
@@ -0,0 +1,45 @@
+package svz.addressbook.tests;
+
+import org.testng.annotations.Test;
+import svz.addressbook.model.GroupData;
+import svz.addressbook.model.Groups;
+
+import java.sql.*;
+
+public class DbConnectionTest {
+
+  @Test
+  public void testDbConnection() {
+    Connection conn = null;
+
+    try {
+      conn = DriverManager.getConnection(
+              "jdbc:mysql://localhost:3306/addressbook?user=root&password=");
+      Statement st = conn.createStatement();
+      ResultSet rs = st.executeQuery("select group_id, group_name, group_header, group_footer from group_list");
+      Groups groups = new Groups();
+      while (rs.next()) {
+        groups.add(new GroupData()
+                .withId(rs.getInt("group_id"))
+                .withName(rs.getString("group_name"))
+                .withHeader(rs.getString("group_header"))
+                .withFooter(rs.getString("group_footer")));
+      }
+      rs.close();
+      st.close();
+      conn.close();
+      
+      System.out.println(groups);
+
+      //Do something with the Connection
+
+    } catch (SQLException ex) {
+
+      // handle any errors
+
+      System.out.println("SQLException: " + ex.getMessage());
+      System.out.println("SQLSatet: " + ex.getSQLState());
+      System.out.println("VendorError: " + ex.getErrorCode());
+    }
+  }
+}
