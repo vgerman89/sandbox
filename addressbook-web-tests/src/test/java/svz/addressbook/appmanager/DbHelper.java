a2e5916c9cecb6e2c5fe9f4adf9c04786876fac6
vgerman <vgermanrus@gmail.com>
2018-09-20 00:42:12 +0300

    Забыл закоммитить метод получения списка контактов

--- addressbook-web-tests/src/test/java/svz/addressbook/appmanager/DbHelper.java
+++ addressbook-web-tests/src/test/java/svz/addressbook/appmanager/DbHelper.java
@@ -42,4 +42,12 @@ public class DbHelper {
     return new Contacts(result);
   }
 
+  public List<ContactData> contactsList() {
+    Session session = sessionFactory.openSession();
+    session.beginTransaction();
+    List<ContactData> result = session.createQuery("from ContactData where deprecated='0000-00-00'").list();
+    session.getTransaction().commit();
+    session.close();
+    return result;
+  }
 }
