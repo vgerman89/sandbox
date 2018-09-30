2d5168ca02a954156cb63f560883fc64d2a6fa78
vgerman <vgermanrus@gmail.com>
2018-09-30 17:37:04 +0300

    Перенс данные о подключении к soap интерфейсу в local.properties

--- mantis-tests/src/test/java/svz/mantis/appmanager/SoapHelper.java
+++ mantis-tests/src/test/java/svz/mantis/appmanager/SoapHelper.java
@@ -22,7 +22,7 @@ public class SoapHelper {
 
   public Set<Project> getProjects() throws MalformedURLException, ServiceException, RemoteException {
     MantisConnectPortType mc = getMantisConnect();
-    ProjectData[] projects = mc.mc_projects_get_user_accessible("administrator", "root");
+    ProjectData[] projects = mc.mc_projects_get_user_accessible(app.getProperty("soap.adminLogin"), app.getProperty("soap.adminPassword"));
     return Arrays.asList(projects).stream()
             .map((p) -> new Project().withId(p.getId().intValue()).withName(p.getName()))
             .collect(Collectors.toSet());
@@ -30,19 +30,19 @@ public class SoapHelper {
 
   private MantisConnectPortType getMantisConnect() throws ServiceException, MalformedURLException {
     return new MantisConnectLocator()
-              .getMantisConnectPort(new URL("http://localhost/mantisbt-2.17.0/api/soap/mantisconnect.php"));
+              .getMantisConnectPort(new URL(app.getProperty("soap.mantisConnectPort")));
   }
 
   public Issue addIssue(Issue issue) throws MalformedURLException, ServiceException, RemoteException {
     MantisConnectPortType mc = getMantisConnect();
-    String[] categories = mc.mc_project_get_categories("administrator", "root", BigInteger.valueOf(issue.getProject().getId()));
+    String[] categories = mc.mc_project_get_categories(app.getProperty("soap.adminLogin"), app.getProperty("soap.adminPassword"), BigInteger.valueOf(issue.getProject().getId()));
     IssueData issueData = new IssueData();
     issueData.setSummary(issue.getSummary());
     issueData.setDescription(issue.getDescription());
     issueData.setCategory(categories[0]);
     issueData.setProject(new ObjectRef(BigInteger.valueOf(issue.getProject().getId()), issue.getProject().getName()));
-    BigInteger issueId = mc.mc_issue_add("administrator", "root", issueData);
-    IssueData createdIssueData = mc.mc_issue_get("administrator", "root", issueId);
+    BigInteger issueId = mc.mc_issue_add(app.getProperty("soap.adminLogin"), app.getProperty("soap.adminPassword"), issueData);
+    IssueData createdIssueData = mc.mc_issue_get(app.getProperty("soap.adminLogin"), app.getProperty("soap.adminPassword"), issueId);
     return new Issue().withId(createdIssueData.getId().intValue())
             .withSummary(createdIssueData.getSummary())
             .withDescription(createdIssueData.getDescription())
