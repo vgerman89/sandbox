e3aad466b7e7f8212b0ec74ea224fb807cb2cab0
vgerman <vgermanrus@gmail.com>
2018-09-30 22:23:08 +0300

    Проверяем статуст таска перед выполнением теста

--- rest-sample/src/main/java/svz/sandbox/rest/Issue.java
+++ rest-sample/src/main/java/svz/sandbox/rest/Issue.java
@@ -5,6 +5,8 @@ public class Issue {
   private int id;
   private String subject;
   private String description;
+  private int state;
+
 
   public int getId() {
     return id;
@@ -33,6 +35,24 @@ public class Issue {
     return this;
   }
 
+  public int getState() {
+    return state;
+  }
+  public Issue withState(int state) {
+    this.state = state;
+    return this;
+  }
+
+  @Override
+  public String toString() {
+    return "Issue{" +
+            "id=" + id +
+            ", subject='" + subject + '\'' +
+            ", description='" + description + '\'' +
+            ", state=" + state +
+            '}';
+  }
+
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
@@ -41,6 +61,7 @@ public class Issue {
     Issue issue = (Issue) o;
 
     if (id != issue.id) return false;
+    if (state != issue.state) return false;
     if (subject != null ? !subject.equals(issue.subject) : issue.subject != null) return false;
     return description != null ? description.equals(issue.description) : issue.description == null;
   }
@@ -50,7 +71,7 @@ public class Issue {
     int result = id;
     result = 31 * result + (subject != null ? subject.hashCode() : 0);
     result = 31 * result + (description != null ? description.hashCode() : 0);
+    result = 31 * result + state;
     return result;
   }
-
 }
--- rest-sample/src/main/java/svz/sandbox/rest/RestAssuredTests.java
+++ rest-sample/src/main/java/svz/sandbox/rest/RestAssuredTests.java
@@ -1,27 +1,31 @@
 package svz.sandbox.rest;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
-import com.google.gson.reflect.TypeToken;
 import com.jayway.restassured.RestAssured;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Set;
 
 import static org.testng.Assert.assertEquals;
 
-public class RestAssuredTests {
+public class RestAssuredTests extends TestBase {
 
   @BeforeClass
   public void init() {
-    RestAssured.authentication = RestAssured.basic("28accbe43ea112d9feb328d2c00b3eed", "");
+    RestAssured.authentication = RestAssured.basic("288f44776e7bec4bf44fdfeb1e646490", "");
   }
 
   @Test
+  public void testIssueIsOpen() throws IOException {
+    Issue issue = getIssues().iterator().next();
+    skipIfNotFixed(issue.getId());
+    Issue newIssue = new Issue().withSubject("Test issue").withDescription("New test issue");
+    int issueId = createIssue(newIssue);
+    System.out.println("Issue created: " + issueId);
+  }
+
+  @Test (enabled =false)
   public void testCreateIssue() throws IOException {
     Set<Issue> oldIssues = getIssues();
     Issue newIssue = new Issue().withSubject("Test issue").withDescription("New test issue");
@@ -30,21 +34,4 @@ public class RestAssuredTests {
     oldIssues.add(newIssue.withId(issueId));
     assertEquals(newIssues, oldIssues);
   }
-
-  private Set<Issue> getIssues() throws IOException {
-    String json = RestAssured.get("http://demo.bugify.com/api/issues.json").asString();
-    JsonElement parsed = new JsonParser().parse(json);
-    JsonElement issues = parsed.getAsJsonObject().get("issues");
-    return new Gson().fromJson(issues, new TypeToken<List<Issue>>(){}.getType());
-  }
-
-  private int createIssue(Issue newIssue) throws IOException {
-    String json = RestAssured.given()
-            .parameter("subject", newIssue.getSubject())
-            .parameter("description", newIssue.getDescription())
-            .post("http://demo.bugify.com/api/issues.json").asString();
-    JsonElement parsed = new JsonParser().parse(json);
-    return parsed.getAsJsonObject().get("issue_id").getAsInt();
-  }
-
 }
