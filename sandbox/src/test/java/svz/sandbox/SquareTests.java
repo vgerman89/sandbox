c650eeb7acc133db2473f8bc06b2c362defb38d3
vgerman <vgermanrus@gmail.com>
2018-10-07 17:12:45 +0300

    используем testUseNG в sandbox тестах

--- sandbox/src/test/java/svz/sandbox/SquareTests.java
+++ sandbox/src/test/java/svz/sandbox/SquareTests.java
@@ -8,8 +8,8 @@ public class SquareTests {
 
   @Test
   public void testArea() {
-//    Square s = new Square(5);
-//    Assert.assertEquals(s.area(), 25.0);
+    Square s = new Square(5);
+    Assert.assertEquals(s.area(), 25.0);
 
 
   }
